c2a8fe516aa08bbc7d6490c0a76f6e06d23c372e
jbdura <bidurajohnson@gmail.com>
2023-10-18 14:30:38 +0300

    Added new data

---
+++ setup_git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create a new directory for your project if needed
+# mkdir your_project_directory
+# cd your_project_directory
+
+#echo "# fun-task2" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin git@github.com:jbdura/fun-task2.git
+#git push -u origin main
+
+git add .
+git commit -m "Added new data"
+git push
